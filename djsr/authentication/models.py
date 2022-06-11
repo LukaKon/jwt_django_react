a4bff238261cd4072dd8e968616253bdfcc6541d
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-06-12 00:01:13 +0200

    add CustomUserSerializer

---
+++ djsr/authentication/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    fav_color=models.CharField(blank=True,max_length=120)
---
+++ djsr/authentication/views.py
@@ -0,0 +1,7 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
+from .serializers import MyTokenObtainPairSerializer
+
+class ObtainTokenPairWithColorView(TokenObtainPairView):
+    permission_classes=(permissions.AllowAny,)
+    serializer_class=MyTokenObtainPairSerializer
