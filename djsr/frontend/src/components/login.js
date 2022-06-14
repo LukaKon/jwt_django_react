fc7cd68b29641b94c46a4c22d5c817fb8f8f56f1
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-06-14 22:01:55 +0200

    add forms

--- djsr/frontend/src/components/login.js
+++ djsr/frontend/src/components/login.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Login = () => {
+    const [form, setForm] = useState({ username: "", password: "" });
+
+    const handleSubmit = (e) => {
+        console.log(`Data submitted: ${form.username} and ${form.password}`);
+        e.preventDefault();
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm((state) => {
+            return { ...state, [name]: value };
+        });
+    };
+    // console.log("form: ", form);
     return (
         <div>
-            <h2>Login page.</h2>
+            Login
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username:
+                    <input
+                        name="username"
+                        type="text"
+                        value={form.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        name="password"
+                        type="password"
+                        value={form.password}
+                        onChange={handleChange}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
--- djsr/frontend/src/components/signup.js
+++ djsr/frontend/src/components/signup.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Signup = () => {
+    const [form, setForm] = useState({ username: "", password: "", email: "" });
+
+    const handleSubmit = (e) => {
+        console.log(
+            `Data submitted: ${form.username}, ${form.email}, ${form.password}`
+        );
+        e.preventDefault();
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm((state) => {
+            return { ...state, [name]: value };
+        });
+    };
     return (
         <div>
-            <h2>Signup page.</h2>
+            Signup
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username{" "}
+                    <input
+                        name="username"
+                        type="text"
+                        value={form.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Email:
+                    <input
+                        name="email"
+                        type="email"
+                        value={form.email}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        name="password"
+                        type="password"
+                        value={form.password}
+                        onChange={handleChange}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
