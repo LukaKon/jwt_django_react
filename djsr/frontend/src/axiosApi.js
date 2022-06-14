c8a05a4c2ec415dd6a86a3e2d46772be1131b5b0
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-06-14 22:15:43 +0200

    add axios instance

---
+++ djsr/frontend/src/axiosApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: "http://128.0.0.1:8000/api/",
+    timeout: 5000,
+    headers: {
+        Authorization: "JWT " + localStorage.getItem("access_token"),
+        "Content-Type": "application/json",
+    },
+});
