c7f242bf9098b6a7b4a311fd49875945728a2dec
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-06-26 21:43:29 +0200

    add: autorefresh token

--- djsr/frontend/src/axiosApi.js
+++ djsr/frontend/src/axiosApi.js
@@ -21,3 +21,34 @@ export const axiosInstance = axios.create({
         accept: "application/json",
     },
 });
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        if (
+            error.response.status === 401 &&
+            error.response.statusText === "Unauthorized"
+        ) {
+            const refresh_token = localStorage.getItem("refresh_token");
+
+            try {
+                const response = await axiosInstance.post("/token/refresh/", {
+                    refresh: refresh_token,
+                });
+                localStorage.setItem("access_token", response.data.access);
+                localStorage.setItem("refresh_token", response.data.refresh);
+
+                axiosInstance.defaults.headers["Authorization"] =
+                    "JWT " + response.data.access;
+                originalRequest.headers["Authorization"] =
+                    "JWT " + response.data.access;
+                return await axiosInstance(originalRequest);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
