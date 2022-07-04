3b2039bf37d792d45764259e0739924b369a4693
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-07-04 06:39:47 +0200

    up

--- djsr/authentication/views.py
+++ djsr/authentication/views.py
@@ -1,17 +1,19 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import permissions,status
-from .serializers import MyTokenObtainPairSerializer,CustomUserSerializer
+from rest_framework import permissions, status
+from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
+
 class ObtainTokenPairWithColorView(TokenObtainPairView):
-    permission_classes=(permissions.AllowAny,)
-    serializer_class=MyTokenObtainPairSerializer
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
 
 class CustomUserCreate(APIView):
-    permission_classes=(permissions.AllowAny,)
-    authentication_classes=()
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
 
     def post(self, request, format='json'):
         serializer = CustomUserSerializer(data=request.data)
@@ -22,19 +24,22 @@ class CustomUserCreate(APIView):
                 return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class LogoutAndBlacklistRefreshTokenForUserView(APIView):
-    permission_classes=(permissions.AllowAny,)
-    authentication_classes=()
 
-    def post(self,request):
+class LogoutAndBlacklistRefreshTokenForUserView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request):
         try:
-            refresh_token=request.data.get('refresh_token')
-            token=RefreshToken(refresh_token)
-            token.blacklist()
+            refresh_token = request.data.get('refresh_token')
+            token = RefreshToken(refresh_token)
+            token.blacklistacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
+        except Exception:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
 class HelloWorldView(APIView):
 
-    def get(self,request):
-        return Response(data={'hello':'world'},status=status.HTTP_200_OK)
+    def get(self, request):
+        return Response(data={'hello': 'world'}, status=status.HTTP_200_OK)
